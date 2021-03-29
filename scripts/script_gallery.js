da8abfa160709d7aace619b95a1441230ada8e00
YannickAiguier <yannick.aiguier@gmail.com>
2021-03-29 16:01:33 +0200

    Feat : la structure de la gallerie est réalisée #26

--- scripts/script_gallery.js
+++ scripts/script_gallery.js
@@ -1,4 +1,9 @@
+// tableau contenant le nom des images
+let images = ["ms01.jpg", "ws01.jpg", "md01.jpg", "wd01.jpg", "mx01.jpg", "mx02.jpg"];
+
 $(document).ready(function () {
+
+    createGallery();
 
     // clic sur le bouton menu : affiche/masque le menu
     $(".menu__btn").on("click", function () {
@@ -15,4 +20,22 @@ $(document).ready(function () {
 // fonction pour afficher/masquer le menu
 function toggleMenu() {
     $(".menu-content").toggleClass("show");
+}
+
+// fonction qui crée la gallerie
+function createGallery() {
+    let newArticle = ("<article>");
+    $("h1").after(newArticle);
+    images.forEach(entity => {
+        createGalleryElement(entity);
+    });
+}
+
+// fonction qui crée une vignette de la gallery
+function createGalleryElement(element) {
+    let newDiv = ("<div>");
+    $("article").append(newDiv);
+    let newImg = ("<img>");
+    $("div:last").append(newImg)
+    $("img:last").attr("src", "images/" + element);
 }
