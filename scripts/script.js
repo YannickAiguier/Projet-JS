e8fe31f6ec308f69f0deb0757db89b4ba1d9a0b1
YannickAiguier <yannick.aiguier@gmail.com>
2021-03-29 09:55:57 +0200

    Feat: le formulaire est créé #22

--- scripts/script.js
+++ scripts/script.js
@@ -27,10 +27,12 @@ $(document).ready(function () {
         home();
     })
 
+    // clic sur le bouton menu : affiche/masque le menu
     $(".menu__btn").on("click", function () {
         toggleMenu();
     })
 
+    // la souris quitte le menu : il disparait
     $(".menu-content").on("mouseout", function () {
         toggleMenu();
     })
@@ -84,6 +86,7 @@ function showResult(data) {
     });
 }
 
+// fonction pour afficher/masquer le menu
 function toggleMenu() {
     $(".menu-content").toggleClass("show");
 }
