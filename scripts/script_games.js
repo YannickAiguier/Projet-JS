227b61b27fcc72574e04334b49150751c31bb287
YannickAiguier <yannick.aiguier@gmail.com>
2021-03-30 19:20:55 +0200

    dev : le clic sur une image affecte cette image au choix du joueur #37

--- scripts/script_games.js
+++ scripts/script_games.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // les listeners
     // clic sur le bouton menu : affiche/masque le menu
     $(".menu__btn").on("click", function () {
         toggleMenu();
@@ -9,10 +10,24 @@ $(document).ready(function () {
     $(".menu-content").on("mouseout", function () {
         toggleMenu();
     })
+
+    // sur les 3 images : clic = afficher l'image dans le cadre joueur
+    $(".scissors, .leaf, .stone").on("click", function() {
+        selectChoice(this, ".userSymbol");
+    })
 });
 
 
 // fonction pour afficher/masquer le menu
 function toggleMenu() {
     $(".menu-content").toggleClass("show");
+}
+
+// fonction pour afficher le symbole choisi dans la case du joueur
+function selectChoice(choice, target) {
+    let imgSrc = choice.querySelector("img").attributes["src"].value;
+    let newImg = ("<img>");
+    $(target).append(newImg);
+    //$(target + ":img").attr("src", imgSrc);
+    $(target + " img").attr("src", imgSrc);
 }
